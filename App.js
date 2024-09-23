a481b4a6ece2a8752d7f2fd894b916615c4a0287
gracemoore1 <159135531+gracemoore1@users.noreply.github.com>
2024-09-23 16:43:36 +0100

    Second commit

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import {View, Text} from 'react-native';
+import {View, TextView, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default function App(){
 
   return(
     <View>
-      <Text> Hello World </Text>
+      <Text style={styles.paragraph}> Hello World </Text>
     </View>
   )
 }
