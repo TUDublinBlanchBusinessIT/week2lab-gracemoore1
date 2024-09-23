5cdd5be91671680bdc88f2c9ad8b318b278489db
gracemoore1 <159135531+gracemoore1@users.noreply.github.com>
2024-09-23 16:55:03 +0100

    Third Commit

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import {View, TextView, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
 
 const styles = StyleSheet.create({
   paragraph: {
@@ -10,10 +11,11 @@ const styles = StyleSheet.create({
 });
 
 export default function App(){
+const [fullname, setFullname] = useState("Grace Moore")
 
   return(
     <View>
-      <Text style={styles.paragraph}> Hello World </Text>
+      <Text style={styles.paragraph}> Hello World {fullname} </Text>
     </View>
   )
 }
